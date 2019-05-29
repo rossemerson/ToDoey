370f19e64833185d05edf397e6619487472322d2
Ross Emerson <remerson@outlook.ie>
2019-05-29 11:39:44 +0100

    Data persistence userDefault code added

--- ToDoey/ToDoListViewController.swift
+++ ToDoey/ToDoListViewController.swift
@@ -11,10 +11,16 @@ import UIKit
 class ToDoListViewController: UITableViewController {
 
     var itemArray = ["Raod Rage", "Mad Max", "Kill Bill"]
+    //create an object to hold access to our userDefaults persistant data
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //check the userDefaults array to see if anything is stored locally in the pList file. if so this is returned as the array.
+        if let item = defaults.array(forKey: "ToDoListArray") as? [String]{
+            itemArray = item
+        }
     }
 
     //MARK: TableView DataSource Methods
@@ -55,6 +61,7 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (alert) in
             // add code to determine what happens once the user presses the UIAction add button
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
         //Create the Label Text field element in the UI with a placeholder
