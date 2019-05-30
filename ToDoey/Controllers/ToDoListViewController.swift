3e6afaa2578d89696898b9983e20458df9cd3d32
Ross Emerson <remerson@outlook.ie>
2019-05-30 12:20:30 +0100

    Change to MVC model - Data model construction for holding the checkmark against the cell item

--- ToDoey/Controllers/ToDoListViewController.swift
+++ ToDoey/Controllers/ToDoListViewController.swift
@@ -10,17 +10,33 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    var itemArray = ["Raod Rage", "Mad Max", "Kill Bill"]
+    //create the array from Item class objects
+    var itemArray = [Item]()
     //create an object to hold access to our userDefaults persistant data
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //create a property that uses the Item Class to hold a new item object (of type Item).
+        let newItem = Item()
+        //create the reference object
+        newItem.title = "Road Rage"
+        //create the code to append the object to the Array
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Kill Bill"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Fat Sam"
+        itemArray.append(newItem3)
+        
         //check the userDefaults array to see if anything is stored locally in the pList file. if so this is returned as the array.
-        if let item = defaults.array(forKey: "ToDoListArray") as? [String]{
-            itemArray = item
-        }
+//        if let item = defaults.array(forKey: "ToDoListArray") as? [String]{
+//            itemArray = item
+//        }
     }
 
     //MARK: TableView DataSource Methods
@@ -28,7 +44,14 @@ class ToDoListViewController: UITableViewController {
     // Declare cellForRowAt indexPath
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        //toggle the checkmark to display or hide when the cell is selected
+        if itemArray[indexPath.row].done == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
         return cell
     }
     
@@ -39,16 +62,11 @@ class ToDoListViewController: UITableViewController {
     
     // MARK: TableView Delegate methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
 
-        //toggle the checkmark to display or hide when the cell is selected
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
-        // code to flash the selected row rather than highlight the row
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
+
+        // code to make the selected row of teh UI flask temporarily rather than premenantly highlight the row
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -60,7 +78,9 @@ class ToDoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Lets add an item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (alert) in
             // add code to determine what happens once the user presses the UIAction add button
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
---
+++ ToDoey/Data Model/Items.swift
@@ -0,0 +1,14 @@
+//
+//  Items.swift
+//  ToDoey
+//
+//  Created by Ross Emerson on 30/05/2019.
+//  Copyright © 2019 Ross Emerson. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
+
